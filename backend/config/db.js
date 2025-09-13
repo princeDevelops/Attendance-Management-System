f5f97bfcb3341d4a1f371640313ab4c129e54449
Prince Kumar <prince.kumar.main.id@gmail.com>
2025-09-13 17:32:57 +0530

    Established Connection To DB

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+import { Pool } from "pg";
+
+const poolConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    database: process.env.DB_NAME,
+};
+
+const connectDB = async () => {
+    const pool = new Pool(poolConfig);
+    try {
+        await pool.connect();
+        console.log("Connected to PostgreSQL");
+        return pool;
+    } catch (err) {
+        console.error("DB Connection Error :", err);
+        throw err;
+    }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,17 @@
-import dotenv from "dotenv";
+import 'dotenv/config'; // loads .env immediately
 import app from "./app.js";
-// import connectDB from "./config/db.js";
-
-
-dotenv.config();
-
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT,()=>{
-    console.log(`server running on http://localhost:${PORT}`);
-});
-
-
-// // connect DB First
-// connectDB()
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log(`🚀 Server running on http://localhost:${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("❌ DB connection failed:", err.message);
-//     process.exit(1);
-//   });
+// connect DB First
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ DB connection failed:", err.message);
+    process.exit(1);
+  });
